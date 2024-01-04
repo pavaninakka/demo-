0591e50236dfecb8b2779f948af7c85192cda900
Admin <Admin@DESKTOP-CTPV5L8>
2024-01-04 11:03:58 +0530

    initial commit

---
+++ src/demo/Months.java
@@ -0,0 +1,25 @@
+package demo;
+
+public class Months {
+	
+	public static void main (String [] args) {
+		
+		int month=12;
+		
+		switch(month) {
+		
+		case 1: System.out.println("Jan"); break;
+		case 2: System.out.println("feb"); break;
+		case 3: System.out.println("mar"); break;
+		case 4: System.out.println("apr"); break;
+		case 5: System.out.println("may"); break;
+		case 6: System.out.println("Jun"); break;
+		case 7: System.out.println("Jul"); break;
+		case 8: System.out.println("aug"); break;
+		case 9: System.out.println("sep");  break;
+		default: System.out.println("invaild number"); 
+		
+		
+	}
+
+}
---
+++ src/demo/Num.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Num {
+
+  public static void main (String [] args)	{
+	  
+	  int number=12;
+	  
+	   if(number %2==0) {
+		   
+		   System.out.println ("Even number");
+	   }
+	   else {
+		   System.out.println ("odd number");
+	   }
+  }
+}
